e2a608d6884a83442ede324317b568299ffc2574
Filipe Cabecinhas <filcab+github@gmail.com>
2015-11-07 16:31:50 +0000

    Added automake-1.14 formula

---
+++ Library/Formula/automake114.rb
@@ -0,0 +1,56 @@
+require 'formula'
+
+class Automake < Formula
+  homepage 'http://www.gnu.org/software/automake/'
+  url 'http://ftpmirror.gnu.org/automake/automake-1.14.1.tar.gz'
+  mirror 'http://ftp.gnu.org/gnu/automake/automake-1.14.1.tar.gz'
+  sha1 '0bb1714b78d70cab9907d2013082978a28f48a46'
+
+  bottle do
+    sha1 "b7f153654e9a99194f13a8da140b3d861f046af0" => :yosemite
+    sha1 '8720e073a828e5b7e29e5cacd01b3ccca88a6d40' => :mavericks
+    sha1 'ed3cba7f2a806a4192e875e809bb73b65a128757' => :mountain_lion
+    sha1 '12d0d7d3b6d31ea4faf9551076225343e9a5af1f' => :lion
+  end
+
+  depends_on 'autoconf' => :run
+
+  keg_only :provided_until_xcode43
+
+  def install
+    ENV['PERL'] = '/usr/bin/perl'
+
+    system "./configure", "--prefix=#{prefix}"
+    system "make install"
+
+    # Our aclocal must go first. See:
+    # https://github.com/Homebrew/homebrew/issues/10618
+    (share/"aclocal/dirlist").write <<-EOS.undent
+      #{HOMEBREW_PREFIX}/share/aclocal
+      /usr/share/aclocal
+    EOS
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      int main() { return 0; }
+    EOS
+    (testpath/"configure.ac").write <<-EOS.undent
+      AC_INIT(test, 1.0)
+      AM_INIT_AUTOMAKE
+      AC_PROG_CC
+      AC_CONFIG_FILES(Makefile)
+      AC_OUTPUT
+    EOS
+    (testpath/"Makefile.am").write <<-EOS.undent
+      bin_PROGRAMS = test
+      test_SOURCES = test.c
+    EOS
+    system bin/"aclocal"
+    system bin/"automake", "--add-missing", "--foreign"
+    system "autoconf"
+    system "./configure"
+    system "make"
+    system "./test"
+  end
+end
